1a47001805499fbf7d92875444ee833dcd6bec58
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-08-19 03:13:47 +0500

    images

---
+++ DiceGame/src/assets/index.js
@@ -0,0 +1,9 @@
+import dices from './Images/dices.png';
+import dice_1 from './Images/dice_1.png';
+import dice_2 from './Images/dice_2.png';
+import dice_3 from './Images/dice_3.png';
+import dice_4 from './Images/dice_4.png';
+import dice_5 from './Images/dice_5.png';
+import dice_6 from './Images/dice_6.png';
+
+export { dices, dice_1, dice_2, dice_3, dice_4, dice_5, dice_6 };

692331d1b1b32d1f342a89ff8cd8a03a7b2677f1
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-08-18 15:27:12 +0500

    tailwind and css files

--- ContactUsPage/tailwind.config.js
+++ ContactUsPage/tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '320px',
+        sm: '375px',
+        sml: '500px',
+        md: '667px',
+        mdl: '768px',
+        lg: '960px',
+        lgl: '1024px',
+        xl: '1280px',
+      },
+      colors: {
+        designColor: '#ff014f',
+      },
+    },
   },
   plugins: [],
-}
+};
+
+
+
